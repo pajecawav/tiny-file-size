32db77cd9722075dd9e74254c37f66416fdba254
pajecawav <pajecawav@gmail.com>
2023-02-10 01:10:39 +0300

    fix: expose getFilesFromGlobs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { getBrotliSize, getGzipSize, getRawSize } from "./compress";
 export { buildJsonReport, buildPrettyReport, humanizeBytes } from "./report";
 export type { PrettyReportOptions } from "./report";
-export { getFileSizes, run } from "./run";
+export { getFilesFromGlobs, getFileSizes, run } from "./run";
 export type { FileSize, FileSizesOptions } from "./run";
